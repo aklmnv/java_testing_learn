0a463a72f5abb431e5b2d74abecf4e5340c6756c
aklmnv <103990235+aklmnv@users.noreply.github.com>
2022-04-25 15:21:14 +0300

    Добавление вычисления расстояния между точками на плоскости

--- sandbox/src/main/java/ru/stqa/jtl/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/jtl/sandbox/MyFirstProgram.java
@@ -11,9 +11,18 @@ public class MyFirstProgram {
 
 		Rectangle r = new Rectangle(4, 6);
 		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b +" = " + r.area());
+
+		Point p1 = new Point(1, 1);
+		Point p2 = new Point(2, 2);
+		System.out.println("Вариант 1. Расстояние между точками (" + p1.x + ", " + p1.y + ") и (" + p2.x + ", " + p2.y + ") = " + String.format("%.2f", distance(p1, p2)).replace(',', '.'));
+		System.out.println("Вариант 2. Расстояние между точками (" + p1.x + ", " + p1.y + ") и (" + p2.x + ", " + p2.y + ") = " + String.format("%.2f", p1.distance(p2)).replace(',', '.'));
 	}
 
 	public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double distance(Point p1, Point p2){
+		return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
 	}
 }
