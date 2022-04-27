f6dc14c593b59f41eab747f02941e9c7f32553fb
aklmnv <103990235+aklmnv@users.noreply.github.com>
2022-04-27 08:41:56 +0300

    Тесты класса Point разнесены на 2 метода

--- sandbox/src/test/java/ru/stqa/jtl/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/jtl/sandbox/PointTests.java
@@ -6,13 +6,16 @@ import org.testng.annotations.Test;
 public class PointTests {
 
     @Test
-    public void testDistance(){
+    public void testDistance1() {
         Point p1 = new Point(0, 1);
         Point p2 = new Point(0, 2);
         Assert.assertEquals(p1.distance(p2), 1.0);
+    }
 
-        p1 = new Point(0, 2);
-        p2 = new Point(3, 2);
+    @Test
+    public void testDistance2(){
+        Point p1 = new Point(0, 2);
+        Point p2 = new Point(3, 2);
         Assert.assertEquals(p1.distance(p2), 3.0);
     }
 }
