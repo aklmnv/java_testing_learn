c338b0e34299e1b714bf199b8a83364da1190919
aklmnv <103990235+aklmnv@users.noreply.github.com>
2022-05-11 16:45:35 +0300

    Добавлено выполнение предусловия для удаления и редактирования группы

--- addressbook-web-tests/src/test/java/ru/stqa/jtl/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jtl/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,16 @@
 package ru.stqa.jtl.addressbook.tests;
 
 import org.testng.annotations.*;
+import ru.stqa.jtl.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
