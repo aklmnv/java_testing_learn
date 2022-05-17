6ed437ad883abd1519763d9e0fa9043b40d37d4b
aklmnv <103990235+aklmnv@users.noreply.github.com>
2022-05-17 16:33:23 +0300

    Добавлена проверка списка групп для теста на создание группы

--- addressbook-web-tests/src/test/java/ru/stqa/jtl/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jtl/addressbook/model/GroupData.java
@@ -3,12 +3,17 @@ package ru.stqa.jtl.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-    private final String id;
+    private int id;
     private final String name;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     private final String header;
     private final String footer;
 
-    public GroupData(String id, String name, String header, String footer) {
+    public GroupData(int id, String name, String header, String footer) {
         this.id = id;
         this.name = name;
         this.header = header;
@@ -16,7 +21,7 @@ public class GroupData {
     }
 
     public GroupData(String name, String header, String footer) {
-        this.id = null;
+        this.id = 0;
         this.name = name;
         this.header = header;
         this.footer = footer;
@@ -43,7 +48,7 @@ public class GroupData {
         return Objects.hash(id, name);
     }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
