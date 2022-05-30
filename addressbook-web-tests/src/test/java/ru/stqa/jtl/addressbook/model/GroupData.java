e11b10a73dbc0b9683d373ca7f88244ca32a6467
aklmnv <103990235+aklmnv@users.noreply.github.com>
2022-05-30 17:45:20 +0300

    Сохранение параметров в формате XML

--- addressbook-web-tests/src/test/java/ru/stqa/jtl/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jtl/addressbook/model/GroupData.java
@@ -1,8 +1,13 @@
 package ru.stqa.jtl.addressbook.model;
 
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
 import java.util.Objects;
 
+@XStreamAlias("group")
 public class GroupData {
+    @XStreamOmitField
     private int id;
     private String name;
     private String header;
--- addressbook-web-tests/src/test/java/ru/stqa/jtl/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jtl/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,7 @@
 package ru.stqa.jtl.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.security.AnyTypePermission;
 import org.testng.annotations.*;
 import ru.stqa.jtl.addressbook.model.GroupData;
 import ru.stqa.jtl.addressbook.model.Groups;
@@ -8,6 +10,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -18,14 +21,18 @@ public class GroupCreationTests extends TestBase{
   @DataProvider
   public Iterator<Object[]> validGroups() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    BufferedReader reader = new BufferedReader(new FileReader(new File("addressbook-web-tests/src/test/resources/groups.csv")));
+    BufferedReader reader = new BufferedReader(new FileReader(new File("addressbook-web-tests/src/test/resources/groups.xml")));
+    String xml = "";
     String line = reader.readLine();
     while (line != null){
-      String[] split = line.split(";");
-      list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      xml += line;
       line = reader.readLine();
     }
-    return list.iterator();
+    XStream xstream = new XStream();
+    xstream.processAnnotations(GroupData.class);
+    xstream.addPermission(AnyTypePermission.ANY);
+    List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+    return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
   }
 
   @Test(dataProvider = "validGroups")
@@ -38,7 +45,7 @@ public class GroupCreationTests extends TestBase{
     assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 
-  @Test
+  @Test(enabled = false)
   public void testBadGroupCreation() throws Exception {
     app.goTo().groupPage();
     Groups before = app.group().all();
