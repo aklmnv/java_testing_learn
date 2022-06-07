3254de74e9741354d14b16c4262eb3cb93e6654c
aklmnv <103990235+aklmnv@users.noreply.github.com>
2022-06-07 10:48:50 +0300

    Проверка списков из БД в тестах групп

--- addressbook-web-tests/src/test/java/ru/stqa/jtl/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jtl/addressbook/appmanager/ApplicationManager.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
@@ -22,6 +21,7 @@ public class ApplicationManager {
     private SessionHelper sessionHelper;
     private ContactHelper contactHelper;
     private String browser;
+    private DbHelper dbHelper;
 
     public ApplicationManager(String browser){
         this.browser = browser;
@@ -31,6 +31,7 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("addressbook-web-tests/src/test/resources/%s.properties", target))));
+        dbHelper = new DbHelper();
         if (browser.equals(BrowserType.CHROME)){
             System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
             wd = new ChromeDriver();
@@ -66,4 +67,6 @@ public class ApplicationManager {
     public ContactHelper contact() {
         return contactHelper;
     }
+
+    public DbHelper db(){return dbHelper;}
 }
