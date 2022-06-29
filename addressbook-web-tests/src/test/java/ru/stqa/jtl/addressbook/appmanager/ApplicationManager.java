e100988ddc9d69da2e1a74d5f4edd31e2475811c
Ana.Kleymenova <9FF%6qk64>
2022-06-29 23:11:32 +0300

    Реализована возможность удаленного запуска

--- addressbook-web-tests/src/test/java/ru/stqa/jtl/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jtl/addressbook/appmanager/ApplicationManager.java
@@ -5,10 +5,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -32,15 +35,21 @@ public class ApplicationManager {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
         dbHelper = new DbHelper();
-        if (browser.equals(BrowserType.CHROME)){
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-            wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.FIREFOX)){
-            System.setProperty("webdriver.firefox.driver", "geckodriver.exe");
-            wd = new FirefoxDriver();
-        } else if (browser.equals(BrowserType.IE)){
-            System.setProperty("webdriver.ie.driver", "IEDriverServer.exe");
-            wd = new InternetExplorerDriver();
+        if ("".equals(properties.getProperty("selenium.server"))) {
+            if (browser.equals(BrowserType.CHROME)) {
+                System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+                wd = new ChromeDriver();
+            } else if (browser.equals(BrowserType.FIREFOX)) {
+                System.setProperty("webdriver.firefox.driver", "geckodriver.exe");
+                wd = new FirefoxDriver();
+            } else if (browser.equals(BrowserType.IE)) {
+                System.setProperty("webdriver.ie.driver", "IEDriverServer.exe");
+                wd = new InternetExplorerDriver();
+            }
+        }else{
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
         wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         groupHelper = new GroupHelper(wd);
